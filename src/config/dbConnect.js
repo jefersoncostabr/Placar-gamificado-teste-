f9107559fbcf099025cced3fef5f12fc41209f6e
jefersoncostabr <jefersonb88@gmail.com>
2025-08-30 13:15:39 -0300

    string teste

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,18 +1,32 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
-const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+// dotenv.config();
+// const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
+// async function conectaNaDatabase() {
+//     try {
+//         mongoose.connect(DB_CONNECTION_STRING);
+//         console.log('Conexão com o MongoDB realizada com sucesso!');
+//         return mongoose.connection;
+//     } catch (error) {
+//         console.error('erro ao conectar ao mongo DB',error);
+//     }
+// }
+
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
 
 async function conectaNaDatabase() {
-    try {
-        mongoose.connect(DB_CONNECTION_STRING
-    );
-        console.log('Conexão com o MongoDB realizada com sucesso!');
-        return mongoose.connection;
-    } catch (error) {
-        console.error('erro ao conectar ao mongo DB',error);
-    }
+  try {
+    mongoose.connect(dbConnectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexão com o MongoDB realizada com sucesso!');
+    return mongoose.connection;
+  } catch (error) {
+    console.error('erro ao conectar ao mongo DB', error);
+  }
 }
 
 export default conectaNaDatabase;
